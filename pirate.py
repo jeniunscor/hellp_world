c27b01bf02b823fa15fbc6b1655361f511209843
Adiletcorn.hub <74392950+jeniunscor@users.noreply.github.com>
2025-05-03 13:59:49 -0500

    Add files via upload
    Module 4: Lab Activity

---
+++ pirate.py
@@ -0,0 +1,7 @@
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ("Arrr!"):
+	print("Go away, pirate.")
+else:
+	print("Greetings, hater of pirates!")
+
+  # Adilet Zhanybekov 05/03/2025
